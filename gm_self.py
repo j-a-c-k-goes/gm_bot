de6108db95e873befac4409328c49d8cb433f8b8
jack <jack@flnpb.com>
2022-03-09 04:48:50 -0500

    bot to self message

---
+++ gm_self.py
@@ -0,0 +1,10 @@
+''' gm bot self messages '''
+def gm_bot_self():
+    output_a = f'gm_bot™\tbooting from terminal'
+    output_b = f'--- going to Trav\'s World Server @ {SERVER}\tworking in {TEST_CHANNEL} ---'
+    outputs = [output_a, output_b]
+    xyz = random.choice(outputs)
+    return xyz
+if __name__ == '__main__':
+    print('--- gmBot to self ---'):
+    gm_bot_self()
