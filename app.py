3afd037b4d04c3c4e97b97283e8f7ad089b5f8c3
jack <jack@flnpb.com>
2022-03-09 05:07:28 -0500

    code sweep and compress, response types now in a list

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import discord
 import logging
 from dotenv import load_dotenv
 import random
+
 ''' content modules '''
+from gm_self import *
 from emojis import *
 from std_greetings import *
 from units_of_time import *
@@ -14,6 +16,7 @@ from emo_ingredients import *
 from boondocks_char import *
 from punc import *
 from global_gm import *
+
 ''' function modules '''
 from vee_gm import *
 from non_us_gm import *
@@ -28,7 +31,7 @@ TOKEN = os.getenv('BOT_TOKEN')
 SERVER = os.getenv('TW_SERVER')
 live = 'TW_CHANNEL'
 test = 'TEST_CHANNEL'
-CHANNEL = os.getenv(test) #TW_CHANNEL #TEST_CHANNEL
+CHANNEL = os.getenv(live) #TW_CHANNEL #TEST_CHANNEL
 TEST_CHANNEL = os.getenv('TEST_CHANNEL')
 BREAD = '<:br3ad:900045472088084510>'
 
@@ -42,19 +45,21 @@ client = discord.Client()
 #handler.setFormatter(logging.Formatter('%(asctimes)s:%(levelname)s:%(name)s: %(message)s'))
 #logger.addHandler(handler)
 
-''' gm bot self messages '''
-def gm_bot_self():
-    output_a = f'gm_bot™\tbooting from terminal'
-    output_b = f'--- going to Trav\'s World Server @ {SERVER}\tworking in {TEST_CHANNEL} ---'
-    outputs = [output_a, output_b]
-    xyz = random.choice(outputs)
-    return xyz
+''' response types / syntax builds '''
+def response_types():
+    response_types = [
+    f'{boondocks_gm(boondocks_characters)} {BREAD}', 
+    f'{vee_gm(emo_ingredients, like_as_with)}',
+    f'{base_gm(general_emojis, standard_greetings, punctuation, to_begin, like_as_with)}',
+    f'{time_gm(units_of_time, like_as_with, general_emojis, emo_ingredients)}',
+    f'{global_gm(non_american_gm, general_emojis)}'
+    ]
+    return response_types
 
 ''' bot finished logging in '''
 @client.event
 async def on_ready():
-    gm_bot_to_self = gm_bot_self()
-    print(gm_bot_to_self)
+    gm_bot_self()
 
 @client.event
 async def on_typing(channel, user, when):
@@ -67,22 +72,22 @@ async def on_typing(channel, user, when):
 async def on_message(message):
     #if message.author == client.user:
     #return
+    response = random.choice(response_types())
     if message.content.startswith('gm'.lower()):
-        await message.reply(f'{boondocks_gm(boondocks_characters)} {BREAD}')
+        await message.reply(response)
     elif message.content.startswith('gm'.upper()):
-        await message.reply(f'{vee_gm(emo_ingredients, like_as_with)}')
+        await message.reply(response)
     elif message.content.startswith('gm'.title()):
-        await message.reply(f'{base_gm(general_emojis, standard_greetings, punctuation, to_begin, like_as_with)}')
+        await message.reply(response)
     elif message.content.startswith('morning'):
-        await message.reply(f'{time_gm(units_of_time, like_as_with, general_emojis, emo_ingredients)}')
+        await message.reply(response)
     elif message.content.startswith('TOD'):
-        await message.reply(f'{global_gm(non_american_gm, general_emojis)}')
+        await message.reply(response)
     print(f'gm bot sent a reply to {SERVER} in {TEST_CHANNEL}')
 
 ''' on disconnect '''
 @client.event
 async def on_disconnect():
-    gm_bot_to_self = gm_bot_self(general_emojis)
     print(f'gm bot is {client.user} from {client.id}, until next time')
 ''' run bot '''
 client.run(TOKEN)
